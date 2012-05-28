de4180b932ca8f1126d4596257d42eb1f989ab11
Reiner Pope <reiner.pope@gmail.com>
2012-05-28 09:12:53 -0700

    Larger BenchRead.hs

--- bench/BenchRead.hs
+++ bench/BenchRead.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 module Main(main) where
 
+import Data.String
 import Control.Monad
 import Control.Applicative
 import Data.Attoparsec.Text hiding (Number)
@@ -12,25 +13,40 @@ import Prelude hiding (Read(..), read)
 import Data.Text(Text)
 import qualified Data.Text as T
 
-data Number = Zero | Suc Number
+data Number = Zero | Suc !Number
   deriving (Generic, Prelude.Show, Prelude.Read)
 
 instance Read Number
 
 main = defaultMain [
+  let 
+    contents :: IsString a => a
+    contents = "Suc (Suc (Suc (Suc (Suc (Suc (Suc (Suc (Suc (Suc (Suc (Suc (Suc (Suc (Suc (Suc (Suc (Suc (Suc (Suc Zero)))))))))))))))))))"
+    contents2 = T.filter (\c -> c /= '(' && c /= ')') contents
+  in bgroup "Suc^20 Zero" [
+     bench "text" $ whnf (read :: Text -> Number) contents,
+     bench "string" $ whnf (Prelude.read :: String -> Number) contents,
+     bench "text-via-string" $ whnf (Prelude.read . T.unpack :: Text -> Number) contents,
+     bench "raw" $ whnf plainRead contents2
+     ],
+     
+  bgroup "Suc (Suc Zero)" [
+     bench "text" $ whnf (read :: Text -> Number) "Suc (Suc Zero)",
+     bench "string" $ whnf (Prelude.read :: String -> Number) "Suc (Suc Zero)",
+     bench "text-via-string" $ whnf (Prelude.read . T.unpack :: Text -> Number) "Suc (Suc Zero)",
+     bench "raw" $ whnf plainRead "Suc Suc Zero"
+     ],
   bgroup "Suc Zero" [
      bench "text" $ whnf (read :: Text -> Number) "Suc Zero",
      bench "string" $ whnf (Prelude.read :: String -> Number) "Suc Zero",
      bench "text-via-string" $ whnf (Prelude.read . T.unpack :: Text -> Number) "Suc Zero",
-     bench "raw" $ whnf plainRead "Suc Zero",
-     bench "slow-raw" $ whnf slowPlainRead "Suc Zero"
+     bench "raw" $ whnf plainRead "Suc Zero"
      ],
   bgroup "Zero" [
      bench "text" $ whnf (read :: Text -> Number) "Zero",
      bench "string" $ whnf (Prelude.read :: String -> Number) "Zero",
      bench "text-via-string" $ whnf (Prelude.read . T.unpack :: Text -> Number) "Zero",
-     bench "raw" $ whnf plainRead "Zero",
-     bench "slow-raw" $ whnf slowPlainRead "Zero"
+     bench "raw" $ whnf plainRead "Zero"
      ]
   ]
 
@@ -40,14 +56,3 @@ plainRead t = case parseOnly p t of
   Right n -> n
  where
    p = skipSpace >> ((Zero <$ string "Zero") <|> (Suc <$> (string "Suc" *> p)))
-
-slowString s = do
-  str <- string s
-  guard (s == str)
-
-slowPlainRead :: Text -> Number
-slowPlainRead t = case parseOnly p t of
-  Left msg -> error msg
-  Right n -> n
- where
-   p = (Zero <$ slowString "Zero") <|> (Suc <$> (slowString "Suc " *> p))
